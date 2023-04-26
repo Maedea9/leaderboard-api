316a0665098826afa1487dd2c7a231713b62a3e9
Maedea9 <melaniecascante.j@gmail.com>
2023-04-26 13:30:19 -0500

    Add html structure and basic style

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-import './styles.css';
+// import './styles.css';
+
+class Scores {
+    constructor(id, name, scores) {
+        this.id = id;
+        this.name =name;
+        this.score = score;
+    }
+}
