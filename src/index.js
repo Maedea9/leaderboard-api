bca270b86beaa65f9969ff1ce07a9931b0123c75
Maedea9 <melaniecascante.j@gmail.com>
2023-04-27 21:09:20 -0500

    Add code for sending and recieving data from api

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import './styles.css';
 import Score from './modules/scores.js';
 
+const newScore = new Score();
 const addScore = document.querySelector('.input-form');
 
 addScore.addEventListener('submit', (e) => {
   e.preventDefault();
-  const newScore = new Score();
-  const name = addScore.name.value;
-  const scoreNum = addScore.score.value;
-  newScore.addScore({ name, scoreNum });
-
-  addScore.name.value = '';
-  addScore.score.value = '';
+  const user = addScore.name.value;
+  const scoreNumber = addScore.score.value;
+  newScore.addScore({ user, scoreNumber });
+  addScore.reset();
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-  newScore.displayScore();
-});
+const refreshScore = document.getElementById('refresh-score');
+refreshScore.addEventListener('click', newScore.getData);
+document.addEventListener('DOMContentLoaded', newScore.displayScore);
